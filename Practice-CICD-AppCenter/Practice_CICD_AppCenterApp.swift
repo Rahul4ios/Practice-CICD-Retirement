5cdfc075acdf92a79e5dfe3a40e8fcee9bfb7334
Rahul4ios <connect2ry4jobs@gmail.com>
2023-07-06 17:07:30 +0530

    Initial Commit

---
+++ Practice-CICD-AppCenter/Practice_CICD_AppCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_CICD_AppCenterApp.swift
+//  Practice-CICD-AppCenter
+//
+//  Created by Rahul Yadav on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Practice_CICD_AppCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
